5ecbaff37451d8c1a29d45f14393856b14fd0b20
Soumyadeep20 <soumyadeeppodder20@gmail.com>
2024-01-12 21:00:18 +0530

    Encapsulation Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
+    public static void main (String[] args)
+    {
+        RWOnly a=new RWOnly();
+        /*a.var=2;
+        System.out.println(a.var);
+        output: java: var has private access in com.driver.RWOnly
+         */
+        a.setter(2);
+        System.out.println(a.getter());//output: 2
+    }
   
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int var;
+    public void setter(int x)
+    {
+        this.var=x;
+    }
+    public int getter()
+    {
+        return this.var;
+    }
+
 }
